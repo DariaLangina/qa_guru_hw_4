958e9a12a2de52d0f3b96d68ef8bf8ca4cc77be4
DariaLangina <dlangina@rvision.ru>
2021-11-24 19:20:10 +0300

    browserSize disabled

--- src/test/java/langina/tests/TestBase.java
+++ src/test/java/langina/tests/TestBase.java
@@ -1,13 +1,12 @@
 package langina.tests;
 
-import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
 
   @BeforeAll
   public static void config() {
-    Configuration.browserSize = "1024x765";
+    //Configuration.browserSize = "1024x765";
   }
 
 }
